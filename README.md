4c799ac44004bc5a67a0318d73a43d30c014bbe8
Jihad Assami <jassami1@hotmail.com>
2021-06-03 22:47:54 -0500

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ## Welcome to Amadon
+Code solution.
+
 There are some serious issues with this code! Follow the instructions below to understand what those issues are, and then figure out how to resolve them.
 
 Instructions
--- poorly_coded_store/views.py
+++ poorly_coded_store/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Order, Product
+from django.db.models import Count, Sum, Min, Max, Avg
 
 def index(request):
     context = {
-        "all_products": Product.objects.all()
+        "all_products": Product.objects.all(),
     }
     return render(request, "store/index.html", context)
 
+def process(request):
+    this_product= Product.objects.get(id= int(request.POST['product_id']))
+    quantity = int(request.POST["quantity"])
+    product_price= float(this_product.price)
+    total_charge = quantity * product_price
+    Order.objects.create(quantity_ordered= quantity, total_price=total_charge)
+    return redirect('/checkout')
+
 def checkout(request):
-    quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
-    total_charge = quantity_from_form * price_from_form
-    print("Charging credit card...")
-    Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    all_items= Order.objects.aggregate(Sum('quantity_ordered'))['quantity_ordered__sum']
+    all_cost= Order.objects.aggregate(Sum('total_price'))['total_price__sum']
+    cost= round(all_cost, 2)
+    context={
+        'items': all_items,
+        'last_order':Order.objects.last(),
+        'total_cost': cost,
+    }
+    return render(request, 'store/checkout.html', context)
